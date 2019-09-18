b59266b10211c91d3dbbc2de850314905269ba7b
parthhartler <parth@hartleylab.com>
2019-09-18 10:10:46 +0530

    redux boiler plate

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React from "react";
 
 class Main extends React.Component {
   render() {
-    return (
-      <div className="parent"> React Boiler Plate </div>
-    );
+    return <div className="parent"> React-Redux Boiler Plate </div>;
   }
 }
 
